83d95b704d5a3d579cd0ebf7c93462ae3dee552b
shreyswaroop <shreyasniraj@gmail.com>
2025-02-07 19:20:44 -0500

    Update my-card and index

--- src/my-card.js
+++ src/my-card.js
@@ -14,26 +14,110 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "Riolu";
+    this.image = "https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokedex/9/93/578px-Riolu_anime.png";
+    this.background = "orange";
+    this.description = "A cute Pokémon known for its loyalty and fighting spirit.";
+    this.buttonText1 = "10 Quick Charge";
+    this.buttonText2 = "Endure";
+    this.link = "#";
   }
+
+  static get properties() {
+    return {
+      title: { type: String },
+      image: { type: String },
+      background: { type: String },
+      description: { type: String },
+      buttonText1: { type: String },
+      buttonText2: { type: String },
+      link: { type: String }
+    };
+  }
+  
 
   static get styles() {
     return css`
       :host {
         display: block;
       }
+
+      .card {
+        width: 200px;
+        height: 400px;
+        background-color: var(--card-background, orange);
+        background-size: cover;
+        background-position: center;
+        border-radius: 10px;
+        padding: 16px;
+        margin: 16px;
+        color: black;
+        text-shadow: 1px 1px 2px white;
+      }
+
+      .card-image{
+        width: 200px;
+        height:150px;
+      }
+
+      .fancy{
+        background-color: purple;
+      }
+
+      .card-title {
+        font-size: 30px;
+        margin-bottom: 12px;
+      }
+
+      .button {
+        margin-top: 50px;
+        padding: 1px 10px;
+        color: black;
+        border: 2px solid black;
+        border-radius: 8px;
+        background: none;
+        cursor: pointer;
+        font-size: 20px;
+        text-shadow: 1px 1px 2px white;
+      }
+
+      .button2{
+        margin-top: 25px;
+        padding: 1px 55px;
+        color: black;
+        border: 2px solid black;
+        border-radius: 8px;
+        background: none;
+        cursor: pointer;
+        font-size: 20px;
+        text-shadow: 1px 1px 2px white;
+      }
+
+      button:focus,
+      button:hover {
+        color: white;
+        background-color: black;
+      }
+
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+      <div class="card" style="background-color: ${this.background};">
+        <h2 class="card-title">${this.title}</h2>
+        <img class="card-image" src="${this.image}" alt="${this.title}">
+        <p>${this.description}</p>
+        <button class="button">
+          <a href="${this.link}" target="_blank">${this.buttonText1}</a>
+        </button>
+        <button class="button2">${this.buttonText2}</button>
+      </div>
+    `;
   }
 
-  static get properties() {
-    return {
-      title: { type: String },
-    };
-  }
+  
 }
 
-globalThis.customElements.define(MyCard.tag, MyCard);
+customElements.define(MyCard.tag, MyCard);
+
